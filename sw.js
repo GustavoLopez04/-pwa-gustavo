c15b6c06964445c3a562764536ad3ee1901fb620
Gustavo Lopez <20193tn081@utez.edu.mx>
2022-10-18 10:14:05 -0500

    rutas agregadas

--- sw.js
+++ sw.js
@@ -3,43 +3,44 @@ const INMUTABLE_CACHE_NAME = 'inmutable-cache-v1.1';
 const DYNAMIC_CACHE_NAME = 'dynamic-cache-v1.1';
 
 
-self.addEventListener('install', (event) =>{
-    console.log('SW: Instalado');
-    const respCache = caches.open(STATIC_CACHE_NAME).then((cache)=>{
-       return cache.addAll([
-            '/-pwa-gustavo/',
-            '/-pwa-gustavo/index.html',
-            'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css',
-            'https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js',
-            'https://img.remediosdigitales.com/e997b7/ducati-panigale-v4-r-2019-005/840_560.jpg',
-            'https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css'
-        ]);
-    });
+self.addEventListener('install', (event) => {
+  console.log('SW: Instalado');
+  const respCache = caches.open(STATIC_CACHE_NAME).then((cache) => {
+    return cache.addAll([
+      '/-pwa-gustavo/',
+      '/-pwa-gustavo/index.html',
+      'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css',
+      'https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js',
+      'https://img.remediosdigitales.com/e997b7/ducati-panigale-v4-r-2019-005/840_560.jpg',
+      'https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css'
+    ]);
+  });
 
 
-    event.waitUntil(respCache);
+  event.waitUntil(respCache);
 });
 
-self.addEventListener('fetch', (event)=>{
-    event.respondWith(
-        caches.match(event.request)
-        .then((response)=>{
-          if(response){
-            return response;
-          }
-          else{
-            return fetch(event.request) // response of requests
-            .then((res)=>{
-              return caches.open(STATIC_CACHE_NAME) //create dynamic cache
-              .then((cache)=>{
-                cache.put(event.request.url,res.clone());
-                return res;
-              })
+self.addEventListener('fetch', (event) => {
+
+
+
+  event.respondWith(async () => {
+    const cachedResponse = await caches.match(event.request);
+
+    if (cachedResponse) {
+      return cachedResponse
+    } else {
+      return fetch(event.request) // response of requests
+        .then((res) => {
+          return caches.open(STATIC_CACHE_NAME) //create dynamic cache
+            .then((cache) => {
+              cache.put(event.request.url, res.clone());
+              return res;
             })
-          }
         })
-        .catch(()=>{})
-      )
+    }
+  })
+
 
 
 })
