a6a887a05461b69a3df23445d8537262b27010aa
Gustavo Lopez <20193tn081@utez.edu.mx>
2022-10-18 09:54:21 -0500

    rutas agregadas

--- sw.js
+++ sw.js
@@ -7,12 +7,12 @@ self.addEventListener('install', (event) =>{
     console.log('SW: Instalado');
     const respCache = caches.open(STATIC_CACHE_NAME).then((cache)=>{
        return cache.addAll([
-            '/',
-            '/index.html',
-            'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css',
-            'https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js',
-            'https://img.remediosdigitales.com/e997b7/ducati-panigale-v4-r-2019-005/840_560.jpg',
-            'https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css'
+            '/-pwa-gustavo',
+            '/-pwa-gustavo/index.html',
+            '/-pwa-gustavo/https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css',
+            '/-pwa-gustavo/https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js',
+            '/-pwa-gustavo/https://img.remediosdigitales.com/e997b7/ducati-panigale-v4-r-2019-005/840_560.jpg',
+            '/-pwa-gustavo/https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css'
         ]);
     });
 
